fd6871ad6851a1c416f4aeb5ea04a870ac2cace6
Fyxren <benweerd@gmail.com>
2024-06-23 21:38:59 +0200

    feat: add footer

---
+++ src/lib/components/elements/footer.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Separator } from '$lib/components/ui/separator';
+	import { Button } from '$lib/components/ui/button';
+	import { Badge } from '$lib/components/ui/badge';
+	import { Github } from 'lucide-svelte';
+
+	// @ts-expect-error
+	// eslint-disble-next-line no-undef
+	const pkg = __PACKAGE__;
+</script>
+
+<Separator />
+
+<footer class="px-5 py-10 lg:px-40">
+	<div class="flex items-center gap-2">
+		<img src="/images/logo.svg" alt="logo" class="h-12 w-12" />
+		<h6 class="mt-2 items-center text-xl font-bold">
+			Pianissimo
+			<Badge variant="secondary">v{pkg.version}</Badge>
+		</h6>
+	</div>
+	<p class="text-muted-foreground">A private, invite-only sheet music platform.</p>
+	<Button variant="outline" class="mt-4" href="https://github.com/Fyxren/pianissimo">
+		<Github class="mr-2 h-4 w-4" />
+		Open Source
+	</Button>
+</footer>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
-	import Navbar from '$lib/components/elements/navbar.svelte';
+	import Footer from '$lib/components/elements/footer.svelte';
+import Navbar from '$lib/components/elements/navbar.svelte';
 </script>
 
 <div>
@@ -7,4 +8,5 @@
 	<div class="px-5 py-10 lg:px-40">
 		<slot />
 	</div>
+	<Footer />
 </div>
