c261456ec80a8fd923ef45158b6237b9dc3c0653
Fyxren <benweerd@gmail.com>
2024-06-24 21:01:02 +0200

    refactor: Update layout component to include AuthCheck

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
-	import Footer from '$lib/components/elements/footer.svelte';
+	import AuthCheck from '$lib/components/atoms/auth-check.svelte';
+import Footer from '$lib/components/elements/footer.svelte';
 import Navbar from '$lib/components/elements/navbar.svelte';
 </script>
 
-<div>
+<AuthCheck>
 	<Navbar />
 	<div class="px-5 py-10 lg:px-40">
 		<slot />
 	</div>
 	<Footer />
-</div>
+</AuthCheck>
