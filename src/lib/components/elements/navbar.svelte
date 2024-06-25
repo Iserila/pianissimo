d252aa3746407f6dcddb38f586fb97cc994d0524
Fyxren <benweerd@gmail.com>
2024-06-25 18:31:49 +0200

    refactor: Add dark mode toggle to navbar

--- src/lib/components/elements/navbar.svelte
+++ src/lib/components/elements/navbar.svelte
@@ -6,6 +6,8 @@
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import * as Avatar from '$lib/components/ui/avatar';
 	import { Button } from '$lib/components/ui/button';
+	import { Laptop, Moon, Sun } from 'lucide-svelte';
+	import { setMode, userPrefersMode } from 'mode-watcher';
 
 	const NavItems = [
 		{
@@ -57,11 +59,58 @@
 					</Avatar.Root>
 				</Button>
 			</DropdownMenu.Trigger>
-			<DropdownMenu.Content class="w-56" align="end">
-				<DropdownMenu.Label class="font-normal">
+			<DropdownMenu.Content class="w-fit" align="end">
+				<DropdownMenu.Label class="flex justify-between gap-10 font-normal">
 					<div class="flex flex-col space-y-1">
 						<p class="text-sm font-medium leading-none">{user?.name}</p>
 						<p class="text-xs leading-none text-muted-foreground">{user?.email}</p>
+					</div>
+					<div class="hidden h-7 items-center gap-2 rounded-md border p-1 shadow md:flex">
+						<fieldset
+							role="group"
+							data-orientation="horizontal"
+							class="flex items-center justify-center gap-1"
+						>
+							<button
+								on:click={() => setMode('light')}
+								data-orientation="horizontal"
+								data-state={$userPrefersMode === 'light' ? 'on' : 'off'}
+								aria-checked={$userPrefersMode === 'light' ? 'on' : 'off'}
+								role="radio"
+								tabindex="0"
+								data-melt-toggle-group-item=""
+								data-toggle-group-item=""
+								class="inline-flex h-[22px] w-[22px] items-center justify-center rounded-sm bg-transparent p-0 text-sm font-medium transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground"
+							>
+								<Sun class="h-3.5 w-3.5" />
+							</button>
+							<button
+								on:click={() => setMode('dark')}
+								data-orientation="horizontal"
+								data-state={$userPrefersMode === 'dark' ? 'on' : 'off'}
+								aria-checked={$userPrefersMode === 'dark' ? 'on' : 'off'}
+								role="radio"
+								tabindex="-1"
+								data-melt-toggle-group-item=""
+								data-toggle-group-item=""
+								class="inline-flex h-[22px] w-[22px] items-center justify-center rounded-sm bg-transparent p-0 text-sm font-medium transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground"
+							>
+								<Moon class="h-3.5 w-3.5" />
+							</button>
+							<button
+								on:click={() => setMode('system')}
+								data-orientation="horizontal"
+								data-state={$userPrefersMode === 'system' ? 'on' : 'off'}
+								aria-checked={$userPrefersMode === 'system' ? 'on' : 'off'}
+								role="radio"
+								tabindex="-1"
+								data-melt-toggle-group-item=""
+								data-toggle-group-item=""
+								class="inline-flex h-[22px] w-[22px] items-center justify-center rounded-sm bg-transparent p-0 text-sm font-medium transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground"
+							>
+								<Laptop class="h-3.5 w-3.5" />
+							</button>
+						</fieldset>
 					</div>
 				</DropdownMenu.Label>
 				<DropdownMenu.Separator />
@@ -82,9 +131,15 @@
 					</DropdownMenu.Group>
 					<DropdownMenu.Separator />
 				{/if} -->
+				<!-- <DropdownMenu.Item class="flex justify-between hover:!bg-inherit">
+					Theme
+					
+				</DropdownMenu.Item> -->
 				<form method="POST" action="/logout">
 					<button type="submit" class="h-full w-full text-start">
-						<DropdownMenu.Item class="hover:!bg-destructive hover:!text-destructive-foreground hover:cursor-pointer">
+						<DropdownMenu.Item
+							class="hover:cursor-pointer hover:!bg-destructive hover:!text-destructive-foreground"
+						>
 							Log Out
 						</DropdownMenu.Item>
 					</button>
